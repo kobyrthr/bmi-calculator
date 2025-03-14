c58b58a537f1e936933363a4c8f7468ebc619242
kobyrthr <kobyrthr@gmail.com>
2025-03-14 10:25:24 +0800

    Initial commit

---
+++ src/app/layout.jsx
@@ -0,0 +1,29 @@
+import { Inter } from 'next/font/google';
+import './globals.css';
+import ClientLayout from './client-layout';
+
+const leagueSpartan = Inter({
+  variable: '--font-inter',
+  subsets: ['latin'],
+});
+
+export const metadata = {
+  title: 'BMI Calculator',
+  description:
+    'Calculate your Body Mass Index (BMI) with our easy-to-use calculator. Get instant results and understand your body composition better with this simple yet effective BMI measurement tool.',
+  icons: {
+    icon: '/logo.png',
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${leagueSpartan.className} antialiased`}>
+        <ClientLayout>
+          <main className="block w-full relative ">{children}</main>
+        </ClientLayout>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,42 @@
+'use client';
+import { CompanyContext } from '@/context/company-context';
+import { useContext, useState } from 'react';
+import { Card, CardContent } from '@/components/ui/card';
+import { Typography } from '@/components/ui/typography';
+import Image from 'next/image';
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
+import { useRouter } from 'next/navigation';
+
+export default function Home() {
+  const router = useRouter();
+  const { searchValue, companies, setCompany, selectedStatuses } =
+    useContext(CompanyContext);
+  const [itemsToShow, setItemsToShow] = useState(9);
+  const filteredData = companies.filter((company) => {
+    const matchesStatus = selectedStatuses
+      ? selectedStatuses.includes(company?.contract)
+      : true;
+
+    const matchesSearch = searchValue
+      ? company.company.toLowerCase().includes(searchValue.toLowerCase())
+      : true;
+
+    return matchesStatus && matchesSearch;
+  });
+
+  const currentItems = filteredData.slice(0, itemsToShow);
+  const hasMore = itemsToShow < filteredData.length;
+  const handleLoadMore = () => {
+    setItemsToShow((prev) => prev + 6);
+  };
+
+  const handleClick = (company) => {
+    setCompany(company);
+    router.push(`/jobs/${company.id}`);
+  };
+
+  return (
+    <div className="-mt-10 grid items-center justify-items-center size-full !pt-0 max-[376px]:p-6 p-12 px-10 lg:px-40 gap-16"></div>
+  );
+}
