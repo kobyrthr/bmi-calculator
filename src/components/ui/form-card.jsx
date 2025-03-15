65adff02a2fc7b2cf487b23affc73145f1077dd6
kobyrthr <kobyrthr@gmail.com>
2025-03-15 20:52:06 +0800

    add information bar

---
+++ src/components/ui/form-card.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Card, CardContent, CardTitle } from './card';
+import { RadioGroup, RadioGroupItem } from './radio-group';
+import { Label } from './label';
+import InputMetric from './input-metric.jsx';
+import { Typography } from './typography';
+import InformationBar from './information-bar.jsx';
+
+const FormCard = () => {
+  return (
+    <Card>
+      <CardContent className="flex flex-col gap-6">
+        <CardTitle>Enter your details below</CardTitle>
+        <RadioGroup defaultValue="metric">
+          <div className="flex justify-between gap-6">
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem value="metric" id="metric" />
+              <Label htmlFor="metric">Metric</Label>
+            </div>
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem value="imperial" id="imperial" />
+              <Label htmlFor="imperial">Imperial</Label>
+            </div>
+          </div>
+        </RadioGroup>
+
+        <div className="flex flex-row justify-between gap-6 mt-4">
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="height" className="text-grey-500 ">
+              <Typography type="preset-7" className="">
+                Height
+              </Typography>
+            </Label>
+            <InputMetric id="height" label="Height" placeholder="0" unit="cm" />
+          </div>
+          <div className="flex flex-col gap-2 text-gre">
+            <Label htmlFor="weight" className="text-grey-500">
+              <Typography type="preset-7" className="">
+                Weight
+              </Typography>
+            </Label>
+            <InputMetric id="weight" label="Weight" placeholder="0" unit="kg" />
+          </div>
+        </div>
+        <InformationBar variant="default" className="w-full max-w-full" />
+        <InformationBar variant="bmi" className="w-full max-w-full" />
+      </CardContent>
+    </Card>
+  );
+};
+
+export default FormCard;
---
+++ src/components/ui/information-bar.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Typography } from './typography';
+import { cva } from 'class-variance-authority';
+
+const informationBarVariants = cva(
+  'p-8 rounded-l-2xl rounded-r-[199px] bg-blue-500 text-white flex', // base styles
+  {
+    variants: {
+      variant: {
+        default: 'flex-col',
+        bmi: 'flex-row',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+    },
+  }
+);
+
+const InformationBar = ({ bmi = 0, variant, className, ...props }) => {
+  return (
+    <div className={informationBarVariants({ variant, className })} {...props}>
+      {variant === 'default' ? (
+        <>
+          <Typography type="preset-4">Welcome!</Typography>
+          <Typography type="preset-7">
+            Enter your height and weight and you'll see your BMI result here
+          </Typography>
+        </>
+      ) : (
+        <>
+          <div className="flex flex-col gap-2 w-1/2">
+            <Typography type="preset-6">Your BMI is...</Typography>
+            <Typography type="preset-1">
+              {bmi === 0 ? bmi : bmi.toFixed(2)}
+            </Typography>
+          </div>
+
+          <Typography type="preset-7 w-1/2">
+            Your BMI suggests you’re a healthy weight. Your ideal weight is
+            between <span className="font-bold">63.3kgs - 85.2kgs</span>.
+          </Typography>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default InformationBar;
