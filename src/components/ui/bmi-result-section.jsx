5e1824792ed87e6e7cee77d50658acd0b6217e52
kobyrthr <kobyrthr@gmail.com>
2025-03-16 22:47:36 +0800

    make bmi-result-section phone responsive

--- src/components/ui/bmi-result-section.jsx
+++ src/components/ui/bmi-result-section.jsx
@@ -7,7 +7,7 @@ import { Typography } from '@/components/ui/typography';
 const BMIResultSection = () => {
   return (
     <section className="pt-0 xl:pt-40 xl:pb-22">
-      <div className="max-xl:gap-18 pr-11 xl:pr-0 xl:mx-auto flex flex-row justify-between items-center w-full mx-auto max-w-[841px] xl:max-w-[1160px] relative">
+      <div className="gap-6 sm:gap-18 sm:pr-11 xl:pr-0 xl:mx-auto flex flex-col sm:flex-row sm:justify-between sm:items-center w-full mx-auto max-w-[841px] xl:max-w-[1160px] relative">
         <Image
           src={LineLeft}
           alt="line-left"
@@ -20,9 +20,9 @@ const BMIResultSection = () => {
           width={564}
           height={533}
           alt="man-eating"
-          className="self-start w-[361px] h-[411px] object-contain xl:h-[533px] xl:w-[564px] -ml-10.5 xl:ml-0"
+          className="self-start w-full sm:w-[361px] sm:h-[411px] object-contain xl:h-[533px] xl:w-[564px] sm:-ml-10.5 xl:ml-0"
         />
-        <div className="flex flex-col max-w-[465px] gap-8 relative self-end pb-11">
+        <div className="flex flex-col px-10 sm:px-0 sm:max-w-[465px] gap-8 relative self-center sm:self-end pb-11">
           <Typography type="preset-2" className="text-blue-900">
             What your BMI result means
           </Typography>
